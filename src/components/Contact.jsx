ced36f540a99f95567cd4c10d640b18e8900dff5
work-norvix <work-norvix@gmail.com>
2026-06-08 16:08:10 -0400

    first commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
           <div className="flex flex-col gap-6">
             <div className="flex items-center gap-4 text-white">
               <Mail className="text-[#B4D0FF] w-5 h-5" strokeWidth={2} />
-              <span className="text-[15px] font-medium">hello@norvix.studio</span>
+              <span className="text-[15px] font-medium">work.norvix@gmail.com</span>
             </div>
             
             <div className="flex items-center gap-4 text-white">
